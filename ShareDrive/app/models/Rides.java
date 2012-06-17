47ad91bf4d9f2974d4a115ea8676995cf78ee0e4
noremy@gmail.com <noremy@gmail.com@e51f5a3b-4797-e566-29a8-0b1cb466e581>
2012-06-17 14:14:55 +0000

    model added

---
+++ ShareDrive/app/models/Rides.java
@@ -0,0 +1,55 @@
+package models;
+
+import java.util.*;
+
+import javax.persistence.*;
+
+import play.data.validation.Required;
+import play.db.jpa.*;
+
+@Entity
+/**
+ * @author Pawel
+ *
+ */
+public class Rides {
+
+	// public User driver (will be created during registration, for now
+	@Required(message = "Name of the driver is required")
+	public String nameOfDriver;
+
+	@Required(message = "Start point is required")
+	public String startPoint;
+
+	@Required(message="Destination university is required")
+	public String destinationUniveristy;
+	
+	@Required(message="Destination campus is required")
+	public String destinationCampus;
+
+	@Required(message="Time of arrival is required")
+	public String timeOfArrival;
+
+	@Required(message="Number of seats")
+	public int numOfSeatsAvailable;
+	
+	public String comments;
+
+	@Required
+	@Temporal(TemporalType.DATE)
+	public Date rideDate;
+
+	public Rides(String nameOfDriver, String startPoint, String destinationUniveristy,
+			String destinationCampus, String timeOfArrival, int numOfSeatsAvailable,
+			String comments, Date rideDate) {
+
+		this.nameOfDriver = nameOfDriver;
+		this.startPoint = startPoint;
+		this.destinationUniveristy = destinationUniveristy;
+		this.destinationCampus = destinationCampus;
+		this.timeOfArrival = timeOfArrival;
+		this.numOfSeatsAvailable = numOfSeatsAvailable;
+		this.comments = comments;
+		this.rideDate = rideDate;
+	}
+}
