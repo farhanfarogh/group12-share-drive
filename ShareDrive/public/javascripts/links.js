c7d695445da3ff2ee0898a1fee7c379ef6045cb1
uniqueraihan007@gmail.com <uniqueraihan007@gmail.com@e51f5a3b-4797-e566-29a8-0b1cb466e581>
2012-06-27 16:38:47 +0000

    display Error messages while registration (task e) is done

--- ShareDrive/public/javascripts/links.js
+++ ShareDrive/public/javascripts/links.js
@@ -29,3 +29,26 @@ function hideSubmenu(){
 	hide('Profile');
 	hide('Help');
 }
+
+function chkPassword(passwordObj, errorDispObj) { //checking password length
+	if(passwordObj.value.length < 5 ) { //show error
+	   errorDispObj.innerHTML = "";
+	   errorDispObj.innerHTML = "Minimum password size is 5";
+	}
+	else { //show nothing
+	   errorDispObj.innerHTML = "";
+	   errorDispObj.innerHTML = "Correct password";
+	}
+}
+
+function varifyPassword(verifyPasswordObj, passwordObj, errorDispObj) { //verify the retype password
+	
+	if(verifyPasswordObj.value == passwordObj.value) { // show nothing as the retype pass match with the pass
+	   errorDispObj.innerHTML = "";
+	   errorDispObj.innerHTML = "Password matched";
+	}
+	else { //show error as retype pass does not match
+	   errorDispObj.innerHTML = "";
+	   errorDispObj.innerHTML = "Your password doesn't match";
+	}
+}
