f7af94ad6135a2104ae7593022d08def2dd6dc56
herr.markus.kraft@googlemail.com <herr.markus.kraft@googlemail.com@e51f5a3b-4797-e566-29a8-0b1cb466e581>
2012-06-28 13:38:53 +0000

    Main Menu changed: now onMouseIn and onMouseOut events used

--- ShareDrive/public/javascripts/links.js
+++ ShareDrive/public/javascripts/links.js
@@ -1,33 +1,11 @@
-var actElem='Start';
-
-function show(elem){
-/*	if(elem == 'Rides') {
-		//window.location.href = 'OfferRide.html';
-	}
-	else if(elem == 'Project') {
-		
-	}
-	else if(elem == 'Help') {
-		
-	}*/
-	if(actElem!='Start'){
-		hide();
-	}
-	actElem=elem;			
+function show(elem){		
 	document.getElementById('Sub' + elem).style.visibility = 'visible';
 	document.getElementById(elem).style.borderBottomWidth = '0';
 }
 
-function hide() {
-	document.getElementById('Sub' + actElem).style.visibility = 'hidden';
-	document.getElementById(actElem).style.borderBottomWidth = '4';
-}
-
-function hideSubmenu(){
-	hide('Start');
-	hide('Rides');
-	hide('Profile');
-	hide('Help');
+function hide(elem){
+	document.getElementById('Sub' + elem).style.visibility = 'hidden';
+	document.getElementById(elem).style.borderBottomWidth = '4px';
 }
 
 function chkPassword(passwordObj, errorDispObj) { //checking password length
